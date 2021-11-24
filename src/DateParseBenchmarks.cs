560681d56793a858dc30442d43c01a570b3cd892
Andr� Castilho <acastilhofilho@outlook.com>
2021-11-24 11:46:49 -0300

    first commit

---
+++ src/DateParseBenchmarks.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkTests
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn()]
+    public class DateParseBenchmarks
+    {
+
+        private const string DateTime = "2021-12-13";
+        private static readonly DateParser Parser = new DateParser();
+
+        [Benchmark(Baseline = true)]
+        public void GetYearFromDateTime()
+        {
+            Parser.GetYearFromDate(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSplit()
+        {
+            Parser.GetYearFromSplit(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSubstring()
+        {
+            Parser.GetYearFromSubstring(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpan()
+        {
+            Parser.GetYearFromSpan(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpanWithManualConversion()
+        {
+            Parser.GetYearFromSpanWiThManualConversion(DateTime);
+        }
+    }
+}
---
+++ src/DateParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BenchmarkTests
+{
+    public class DateParser
+    {
+        public int GetYearFromDate(string dateTimeAsString)
+        {
+            var dateTime = DateTime.Parse(dateTimeAsString);
+            return dateTime.Year;
+        }
+
+        public int GetYearFromSplit(string dateTimeAsString)
+        {
+            var splitOnHyphen = dateTimeAsString.Split('-');
+
+            return int.Parse(splitOnHyphen[0]);
+        }
+
+        public int GetYearFromSubstring(string dateTimeAsString)
+        {
+            var indexOnHyphen = dateTimeAsString.IndexOf('-');
+            return int.Parse(dateTimeAsString.Substring(0, indexOnHyphen));
+        }
+
+        public int GetYearFromSpan(ReadOnlySpan<char> dateTimeAsSpan)
+        {
+            var indexOnHyphen = dateTimeAsSpan.IndexOf('-');
+
+            return int.Parse(dateTimeAsSpan.Slice(0, indexOnHyphen));
+
+        }
+
+        public int GetYearFromSpanWiThManualConversion(ReadOnlySpan<char> dateTimeAsSpan)
+        {
+            var indexOnHyphen = dateTimeAsSpan.IndexOf('-');
+            var yearAsSpan = dateTimeAsSpan.Slice(0, indexOnHyphen);
+
+            var temp = 0;
+
+            for (int i = 0; i < yearAsSpan.Length; i++)
+            {
+                temp = temp * 10 + (yearAsSpan[i] - '0');
+            }
+          
+            return temp;
+        }
+
+    }
+}
